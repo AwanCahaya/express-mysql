e642679241aae26e33567744712b96399d6302d8
AwanCahaya <darmawan25yes@outlook.co.id>
2026-01-05 20:16:57 +0700

    membenahi kode dengan menyimpan data penting ke .env

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 // ini untuk kodensi ke database
 const mysql = require("mysql2/promise");
 const dbPool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "express_mysql",
-  password: "",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.PASSWORD,
 });
 module.exports = dbPool;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 5000;
 //untuk memanggil fungsi/middleware lain
 const usersRouters = require("./routes/users");
+//ini contoah menggunakan disturcturing sebenarnya sama mengambil dari controller seperti "/user"
 const { getAllUsers, createAllUsers } = require("./controller/users");
 const logRequest = require("./middleware/logs");
 //memanggil middleware
@@ -14,6 +17,6 @@ app.use("/users", usersRouters);
 //ini kode untuk mengambil data dari data base execute untuk query
 app.get("/", getAllUsers);
 app.post("/", createAllUsers);
-app.listen(4000, () => {
-  console.log("Server Sudah Berjalan di Port 4000");
+app.listen(PORT, () => {
+  console.log(`Server Sudah Berjalan di Port ${PORT}`);
 });
